69eecca38abbfa501bba8b1896bad0defc04c9a8
twfjcn <127495012+twfjcn@users.noreply.github.com>
2026-09-01 21:38:24 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.proxyctrl"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.proxyctrl"
         minSdk = 31
@@ -12,19 +14,28 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    lint {
+        disable += "ExpiredTargetSdkVersion"
+        abortOnError = false
+    }
+
     compileOptions {
         sourceCompatibility = org.gradle.api.JavaVersion.VERSION_1_8
         targetCompatibility = org.gradle.api.JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildTypes {
         release {
             isMinifyEnabled = false
         }
     }
 }
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
 }
